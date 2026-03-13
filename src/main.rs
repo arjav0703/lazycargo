33faa48f18ca779d4f9f1ad468c57a56ee116e81
Arjav Jain <arjavjain0703@gmail.com>
2026-03-13 17:09:36 +0530

    add tui-input crate

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,17 @@ mod app;
 mod domain;
 mod verify;
 use app::App;
-use domain::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
 
-    let mut app = App::new();
-
-    let dependencies = app.manifest.clone().get_dependencies();
+    let app = App::new();
 
     let exit_code = app.run(terminal).await;
 
     ratatui::restore();
 
-    dbg!("Dependencies: {:#?}", dependencies);
-
     std::process::exit(exit_code?);
-    // Ok(())
 }
